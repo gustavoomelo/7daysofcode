5c6d92f2e1cc8787a533fc917a23210e6d9ebcef
gustavoomelo <gustavoom88@gmail.com>
2023-05-25 15:16:51 -0300

    Create main.js

---
+++ day2/main.js
@@ -0,0 +1,17 @@
+let nome = prompt('Qual é o seu nome?');
+let idade = prompt('Qual é a sua idade?');
+let linguagem = prompt('Qual linguagem de programação você está estudando?');
+
+alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}.`);
+
+let gostar = prompt(`Você gosta de estudar ${linguagem}? (Responda com o número 1 para SIM ou 2 para Não)`);
+
+if (gostar == 1){
+    alert('Muito bom! continue estudando e você terá muito sucesso.');
+} else {
+    if (gostar == 2){
+        alert('Ahh que pena... Já tentou arender outras linguagens?');    
+    } else {
+        alert('Sua resposta não foi uma escolha válida!');
+    }
+}
